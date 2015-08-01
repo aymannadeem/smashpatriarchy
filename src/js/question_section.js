4ef441e1af1001ff6ce1492e61431599ca71cec3
Kevin Barabash <kevinb@khanacademy.org>
2015-07-31 20:29:11 -0700

    reworked question_section.js to use flexbox
    Test Plan:
    - npm run watch
    - open index.html

--- src/js/question_section.js
+++ src/js/question_section.js
@@ -1,73 +1,87 @@
 let React = require("react");
-let classNames = require('classnames');
+let classNames = require("classnames");
 
 let styles = require("./styles.js");
 
-let InfoSection = React.createClass({
+let QuestionSection = React.createClass({
     render() {
-        let style = Object.assign({
-            background: "lightyellow"
-        }, styles.section);
         
-        let radius = 50;
-        let round = {
-            width: 2 * radius,
-            height: 2 * radius,
-            lineHeight: 2 * radius + "px",
-            textAlign: "center",
-            backgroundColor: '#9CF',
-            color: "white",
-            borderRadius: radius + 10,
-            //border: "solid 5px #66F",
-            boxSizing: "content-box",
-            fontSize: 60
-        };
-        let roundA = Object.assign({}, round, { float: 'left' });
-        let roundB = Object.assign({}, round, { float: 'right' });
-        
-        let imageStyle = {
-            width: 200,
-            height: 300,
-            border: "solid 1px gray",
-            margin: "auto",
-            display: "block",
-            backgroundPosition: "center center",
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-            backgroundImage: `url('${this.props.image}');`
+        let sectionStyle = {
+            height: '100%',
+            width: '100%',
+            backgroundColor: 'lightyellow',
+            display: 'flex',
+            flexDirection: 'column'
         };
 
-        let buttonClass = classNames({
-            "btn": true,
-            "btn-default": true,
-            "page-scroll": true
-        });
+        let containerStyle = {
+            margin: '50px 0 50px 100px',
+            flexGrow: 1,
+            display: 'flex',
+            flexDirection: 'row'
+        };
+
+        let leftColStyle = {
+            flexGrow: 0,
+            flexShrink: 0,
+            width: 350
+        };
+
+        let rightColStyle = {
+            position: 'relative',
+            flexGrow: 1,
+            flexShrink: 0,
+            display: 'flex',
+            flexDirection: 'column',
+            overflow: 'hidden'
+        };
+
+        let bottomStyle = {
+            position: 'absolute',
+            bottom: 0
+        };
+
+        let imgStyle = {
+            width: 200,
+            height: 300,
+            display: 'block',
+            margin: 'auto',
+            backgroundPosition: 'center center',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            backgroundImage: "url('http://www.weirdasianews.com/wp-content/uploads/2009/12/domo_kun.jpg');"
+        };
+
+        let ageStyle = { 
+            textAlign: 'center',
+            fontSize: '30',
+            fontWeight: 'bold'
+        };
         
-        return <section style={style} id={this.props.id}>
-            <div className={"container"}>
-                <div className={"row"}>
-                    <div className={"col-xs-4"}>
-                        <div className={"woman-at-age-x"} style={imageStyle} />
-                        <div style={{textAlign:"center", fontSize:30}}>
-                            Age<br />X
-                        </div>
-                        <div>{this.props.discourageText}</div>
-                        <div>{this.props.fightText}</div>
+        return <div style={sectionStyle} id={this.props.id}>
+            <div style={containerStyle}>
+                <div style={leftColStyle}>
+                    <div style={imgStyle}></div>
+                    <div style={ageStyle}>Age<br />5</div>
+                        <h3>Factors that Discourage STEM</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pulvinar aliquam leo non eleifend. Pellentesque viverra suscipit purus rutrum porta.
+                        </p>
+                        <h3>How to Fight the Above Factor</h3>
+                        <p>
+                            Sed euismod condimentum nisl non tincidunt. Sed egestas sapien vitae rhoncus molestie. Sed id fermentum arcu. Nullam consequat quam eget lectus fringilla, non vulputate erat gravida.
+                        </p>
                     </div>
-                    <div className={"col-xs-8"}>
-                        <div style={{ marginLeft: 50, marginRight:50, height: 100, zIndex:-1 }}>
-                            <div style={roundA}>A</div>
-                            <div style={roundB}>B</div>
-                        </div>
+                    <div style={rightColStyle}>
+                        <h1>TITLE</h1>
+                        <p style={{ width: 500 }}>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pulvinar aliquam leo non eleifend. Pellentesque viverra suscipit purus rutrum porta. Sed euismod condimentum nisl non tincidunt. Sed egestas sapien vitae rhoncus molestie. Sed id fermentum arcu. Nullam consequat quam eget lectus fringilla, non vulputate erat gravida. Fusce ut vestibulum nunc. In sodales turpis enim, nec bibendum est gravida feugiat.
+                        </p>
                         {this.props.dataviz}
                     </div>
                 </div>
-                <div className={"row"} style={{textAlign:"center",marginTop:25}}>
-                    <a className={buttonClass} href={this.props.nextHref}>Next</a>
-                </div>
-            </div>
-        </section>
+            </div>;
     }
 });
 
-module.exports = InfoSection;
+module.exports = QuestionSection;
